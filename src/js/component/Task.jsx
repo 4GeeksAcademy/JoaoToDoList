b1858fbe2a2bc6afb7317d1e772ed94ef9c30ba7
joaojcfr <joao.ferreira95@yahoo.com.br>
2024-07-06 14:28:25 +0000

    First Commit

---
+++ src/js/component/Task.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+export const Task = (props) => {
+
+    return (
+        <div>
+            <li>
+                <p>something here</p>
+            </li>
+        </div>
+    )
+}
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,47 @@
-import React from "react";
+import React, { useState } from 'react';
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
 
-//create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
-};
+  const handleAddTodo = () => {
+    if (newTodo.trim()) {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
 
-export default Home;
+  const handleDeleteTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div className="main-frame">
+      <div className="title">
+        <h1>To-Do List</h1>
+      </div>
+      
+      <input
+        type="text"
+        placeholder="Add a new task"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        onKeyPress={(e) => e.key === 'Enter' && handleAddTodo()}
+      />
+      <ul>
+        {todos.map((todo, index) => (
+          <>
+          <li key={index}>
+            {todo}
+            <button onClick={() => handleDeleteTodo(index)}>X</button>
+          </li>
+          <hr></hr>
+          </>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default App;
